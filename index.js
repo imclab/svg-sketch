84382f700e543c3ee4b9b563d67e068d1a59247b
Srirangan <srirangan@gmail.com>
2013-12-04 13:42:14 +0100

    Draw rects and lines

--- index.js
+++ index.js
@@ -4,32 +4,12 @@
     var svg = document.getElementById("svg"),
         elements = document.getElementById("elements"),
         toolbar = document.getElementById("toolbar"),
+        do_nothing = function (e) {
+            e.preventDefault();
+            return false;
+        },
         modes = {
-            pen:    {
-                is_drawing:  false,
-                element:     null,
-                element_d:   "",
-                mousedown:   function (e) {
-                    this.is_drawing = true;
-                    this.element = document.createElementNS("http://www.w3.org/2000/svg", "path");
-                    elements.appendChild(this.element);
-                    this.element_d = " M " + e.offsetX + ", " + e.offsetY;
-                    this.element.setAttribute("d", this.element_d);
-                },
-                mousemove:   function (e) {
-                    if (!this.is_drawing) return;
-                    this.element_d += " L " + e.offsetX + ", " + e.offsetY;
-                    this.element.setAttribute("d", this.element_d);
-                },
-                mouseup:     function () {
-                    this.is_drawing = false;
-                },
-                contextmenu: function (e) {
-                    e.preventDefault();
-                    return false;
-                }
-            },
-            select: {
+            select:  {
                 is_moving:           false,
                 move_start_position: null,
                 element:             null,
@@ -37,7 +17,7 @@
                     if (this.is_moving) {
                         var tx = e.offsetX - this.move_start_position.x,
                             ty = e.offsetY - this.move_start_position.y;
-                        this.element.setAttribute("transform", "translate(" + tx + "," + ty + ")")
+                        this.element.setAttribute("transform", "translate(" + tx + "," + ty + ")");
                     }
                     else {
                         var target = e.target,
@@ -71,19 +51,104 @@
                         this.move_start_position = null;
                     }
                 },
-                contextmenu:         function (e) {
-                    e.preventDefault();
-                    return false;
-                }
+                contextmenu:         do_nothing
+            },
+            pen:     {
+                is_drawing:  false,
+                element:     null,
+                element_d:   "",
+                mousedown:   function (e) {
+                    this.is_drawing = true;
+                    this.element = document.createElementNS("http://www.w3.org/2000/svg", "path");
+                    elements.appendChild(this.element);
+                    this.element_d = " M " + e.offsetX + ", " + e.offsetY;
+                    this.element.setAttribute("d", this.element_d);
+                },
+                mousemove:   function (e) {
+                    if (!this.is_drawing) {
+                        return;
+                    }
+                    this.element_d += " L " + e.offsetX + ", " + e.offsetY;
+                    this.element.setAttribute("d", this.element_d);
+                },
+                mouseup:     function () {
+                    if (!this.is_drawing) {
+                        return;
+                    }
+                    this.is_drawing = false;
+                    this.element = null;
+                    this.element_d = "";
+                },
+                contextmenu: do_nothing
+            },
+            line_to: {
+                is_drawing:  false,
+                element:     null,
+                mousedown:   function (e) {
+                    this.is_drawing = true;
+                    this.element = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                    this.element.setAttribute("x1", e.offsetX);
+                    this.element.setAttribute("y1", e.offsetY);
+                    this.element.setAttribute("x2", e.offsetX);
+                    this.element.setAttribute("y2", e.offsetY);
+                    elements.appendChild(this.element);
+                },
+                mousemove:   function (e) {
+                    if (!this.is_drawing) {
+                        return;
+                    }
+                    this.element.setAttribute("x2", e.offsetX);
+                    this.element.setAttribute("y2", e.offsetY);
+                },
+                mouseup:     function (e) {
+                    if (!this.is_drawing) {
+                        return;
+                    }
+                    this.is_drawing = false;
+                    this.element = null;
+                },
+                contextmenu: do_nothing
+            },
+            rect:    {
+                is_drawing:     false,
+                element:        null,
+                element_origin: "",
+                mousedown:      function (e) {
+                    this.is_drawing = true;
+                    this.element = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+                    this.element_origin = e.offsetX + "," + e.offsetY;
+                    this.element.setAttribute("points", this.element_origin);
+                    elements.appendChild(this.element);
+                },
+                mousemove:      function (e) {
+                    if (!this.is_drawing) {
+                        return;
+                    }
+                    var origin = this.element_origin.split(","),
+                        points = this.element_origin +
+                            " " + (e.offsetX) + "," + origin[1] +
+                            " " + e.offsetX + "," + e.offsetY +
+                            " " + origin[0] + "," + e.offsetY;
+                    this.element.setAttribute("points", points);
+                },
+                mouseup:        function (e) {
+                    if (!this.is_drawing) {
+                        return;
+                    }
+                    this.is_drawing = false;
+                    this.element = null;
+                    this.element_origin = "";
+                },
+                contextmenu:    do_nothing
             }
         };
 
-    svg.addEventListener("mousedown", process);
-    svg.addEventListener("mousemove", process);
-    svg.addEventListener("mouseup", process);
-    svg.addEventListener("contextmenu", process);
+    svg.addEventListener("mousedown", trigger_event);
+    svg.addEventListener("mousemove", trigger_event);
+    svg.addEventListener("mouseup", trigger_event);
+    svg.addEventListener("contextmenu", trigger_event);
 
-    function process(e) {
+    function trigger_event(e) {
         var event = e.type,
             mode = toolbar.mode.value;
         svg.setAttribute("class", mode);
